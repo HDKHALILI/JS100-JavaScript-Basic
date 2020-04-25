4088e8794021d34d3d8b7339afc943bf9260bcb0
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-04-25 15:55:56 +1000

    add loops exercises

---
+++ loops/tripple_greeting.js
@@ -0,0 +1,12 @@
+let greeting = 'Aloha!';
+// for loop
+for (let i = 0; i < 3; i += 1) {
+  console.log(greeting);
+}
+
+// while loop
+let counter = 0;
+while (counter < 3) {
+  console.log(greeting);
+  counter += 1;
+}
